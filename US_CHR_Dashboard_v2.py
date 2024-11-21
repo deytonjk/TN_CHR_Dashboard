3c3b06da747a2e989e34ff5037be94e448c62586
Dirtwaterfox <joshuakdeyton@gmail.com>
2024-11-21 00:19:06 -0500

    Update US_CHR_Dashboard_v2.py

--- US_CHR_Dashboard_v2.py
+++ US_CHR_Dashboard_v2.py
@@ -82,7 +82,7 @@ def state_abbr(state_name):
 
 
 #user needs to pick a state to get the list of counties in that state
-states_list = ["United States", "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", 
+states_list = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", 
                "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", 
                "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", 
                "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", 
