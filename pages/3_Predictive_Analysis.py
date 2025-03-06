ebe284f958735fbbf6492378c32b8cc3ad4f889d
Dirtwaterfox <joshuakdeyton@gmail.com>
2025-03-05 22:19:22 -0500

    Add files via upload

---
+++ pages/3_Predictive_Analysis.py
@@ -0,0 +1,18 @@
+
+#This page will let the user choose features to run through some ml models
+
+import streamlit as st
+
+
+#PAGE TITLE
+st.set_page_config(
+    page_title="US County Health Data",
+        page_icon=":hospital",
+        layout="wide"
+)
+st.title("Predictive Analysis")
+
+
+
+
+st.header("Page Under Construction.  Please check back later!")
